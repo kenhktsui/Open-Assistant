29269b56189ffb167744f92a05fdbfba82f0d3c0
Ken Tsui <kenhktsui@gmail.com>
2023-02-12 23:41:49 +0800

    add ReplacedTokenScorer in llmdq

--- data-analysis/llm-data-quality/llmdq/scorer/implementation.py
+++ data-analysis/llm-data-quality/llmdq/scorer/implementation.py
@@ -1,7 +1,11 @@
+from typing import Iterable, List
 from tqdm import tqdm
 from evaluate import load
 from datasets import Dataset
 from transformers.pipelines.pt_utils import KeyDataset
+from transformers import ElectraForPreTraining, ElectraTokenizerFast
+import torch
+import numpy as np
 from llmdq.scorer.base import ScorerBase, HFPipelineScorerBase
 
 
@@ -84,6 +88,43 @@ class ContradictionScorer(HFPipelineScorerBase):
                         total=len(instructanswer_dataset), desc=self.__class__.__name__):
             output.append(self.score_processing(out))
         instructanswer_dataset = instructanswer_dataset.remove_columns("text")
+        instructanswer_dataset = instructanswer_dataset.add_column(f"{self._score_id}_score", output)
+        instructanswer_dataset = instructanswer_dataset.add_column(f"{self._score_id}_model_id", [self._model_id] * len(output))
+        return instructanswer_dataset
+
+
+class ReplacedTokenScorer(ScorerBase):
+    def __init__(self, score_id, model_id, max_length=512, batch_size=8, device=-1):
+        self._score_id = score_id
+        self._model_id = model_id
+        self._discriminator = ElectraForPreTraining.from_pretrained(model_id)
+        self._tokenizer = ElectraTokenizerFast.from_pretrained(model_id)
+        self._max_length = max_length
+        self._batch_size = batch_size
+        self._device = device
+        self._device_pt = f"cuda:{self._device}" if self._device >= 0 else "cpu"
+        self._discriminator.to(self._device_pt)
+
+    def _run_discriminator(self, text_batch: List[str]):
+        prob_list = []
+        inputs = self._tokenizer(text_batch, return_tensors="pt", truncation=True, padding=True,
+                                 max_length=self._max_length).to(self._device_pt)
+        discriminator_outputs_real = self._discriminator(**inputs)
+        for prob in torch.sigmoid(discriminator_outputs_real.logits).cpu().detach().tolist():
+            prob_list.append(np.mean(prob))
+        return prob_list
+
+    def _batching(self, iterable: list) -> Iterable:
+        length = len(iterable)
+        for ndx in range(0, length, self._batch_size):
+            yield iterable[ndx:min(ndx + self._batch_size, length)]
+
+    def score(self, instructanswer_dataset: Dataset) -> Dataset:
+        output = []
+        for d in tqdm(self._batching(instructanswer_dataset),
+                      desc=self.__class__.__name__,
+                      total=len(instructanswer_dataset)//self._batch_size+1):
+            output.extend(self._run_discriminator(d["answer"]))
         instructanswer_dataset = instructanswer_dataset.add_column(f"{self._score_id}_score", output)
         instructanswer_dataset = instructanswer_dataset.add_column(f"{self._score_id}_model_id", [self._model_id] * len(output))
         return instructanswer_dataset
